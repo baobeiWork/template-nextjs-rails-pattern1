fdf4286379e58524510a4d29d1e579af782fc199
TERUTARO <asasa3454@gmail.com>
2025-04-18 18:50:16 +0900

    Makefile追加（Common）

--- make/common.mk
+++ make/common.mk
@@ -6,7 +6,6 @@ SYSTEM_NAME ?= $(shell grep ^SYSTEM_NAME .env | cut -d '=' -f2 | tr -d '"')
 DOCKER_BACKEND_CONTAINER := $(SYSTEM_NAME)_backend
 DOCKER_FRONTEND_CONTAINER := $(SYSTEM_NAME)_frontend
 
-
 build: ## docker-compose buildを実行
 	docker-compose build --no-cache
 
