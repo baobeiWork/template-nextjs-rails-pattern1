0a362e6cb20cdf6ecba8c61ea873c10fad22916a
TERUTARO <asasa3454@gmail.com>
2025-04-29 13:14:51 +0900

    be-init時にGemfileが上書きされる問題を解消

--- make/backend.mk
+++ make/backend.mk
@@ -1,37 +1,39 @@
-reset-be: ## backend ディレクトリをリセット [ Gemfile / Gemfile.lock 保持 ] ()
+be-reset: ## backend ディレクトリをリセット [ Gemfile / Gemfile.lock 保持 ] ()
 	find ./backend -mindepth 1 ! \
 		-name 'Gemfile' ! \
 		-name 'Gemfile.lock' \
 		-exec rm -rf {} +
 
-init-be: ## backend 初期構築 [ Rails new apiモード ] ()
+be-init: ## backend 初期構築 [ Rails new apiモード ] ()
 	@echo "backend 初期化を実行します"
-	docker exec $(DOCKER_BACKEND_CONTAINER) rails new . \
+	docker exec $(DOCKER_BACKEND_CONTAINER) rails new initial-temp \
 	--api \
 	--database=mysql \
 	--skip-javascript \
 	--skip-hotwire \
 	--skip-asset-pipeline \
 	--force
-
-	docker exec $(DOCKER_BACKEND_CONTAINER) bundle install
-
 	docker exec $(DOCKER_BACKEND_CONTAINER) rm -rf \
-		.github \
-		Dockerfile \
-		README.md \
-		test \
-		app/assets \
-		app/helpers \
-		script \
-		.kamal \
-		.dockerignore \
-		.gitattributes \
-		.gitignore
-
+		initial-temp/.github \
+		initial-temp/Dockerfile \
+		initial-temp/README.md \
+		initial-temp/test \
+		initial-temp/app/assets \
+		initial-temp/app/helpers \
+		initial-temp/script \
+		initial-temp/.kamal \
+		initial-temp/.dockerignore \
+		initial-temp/.gitattributes \
+		initial-temp/.gitignore \
+		initial-temp/Gemfile \
+		initial-temp/Gemfile.lock
+	docker exec $(DOCKER_BACKEND_CONTAINER) /bin/bash -c 'mv /backend/initial-temp/* backend/'
+	docker exec $(DOCKER_BACKEND_CONTAINER) rmdir initial-temp
+	docker exec $(DOCKER_BACKEND_CONTAINER) bundle install
+	
 	@echo "初期化完了しました"
 
-switch-entrypoint-be: ## backend entrypoint.shを切り替え ()
+be-switch-entrypoint: ## backend entrypoint.shを切り替え ()
 ifndef type
 	$(error 切り替えるentrypoint typeが指定されていません: make be-switch-entrypoint type=initial または type=deploy)
 endif
